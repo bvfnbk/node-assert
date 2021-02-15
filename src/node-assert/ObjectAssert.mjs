674a06e1b791e24306539df21c8a792623e68bfb
bvfnbk <bvfnbk@web.de>
2021-02-15 20:43:20 +0100

    #5 Adds hasProperty()

--- src/node-assert/ObjectAssert.mjs
+++ src/node-assert/ObjectAssert.mjs
@@ -1,3 +1,5 @@
+import {PropertyError} from './error/index.mjs';
+
 class ObjectAssert {
   /**
    * Asserts that the type of the given `value` is an object.
@@ -13,6 +15,26 @@ class ObjectAssert {
 
     this.core = core;
     this.value = value;
+  }
+
+  /**
+   * Asserts that the wrapped object contains an own property with the given name.
+   *
+   * @param {string} value the name of the property.
+   * @returns {ObjectAssert} the current instance.
+   * @throws {UndefinedArgumentError} if the given value is not defined.
+   * @throws {NullArgumentError} if the given value is `null`.
+   * @throws {TypeConstraintError} if the given value is no `string`
+   * @throws {PropertyError} if the wrapped object does not contain a property with the given name.
+   */
+  hasProperty(value) {
+    this.core.assertString(value);
+
+    if (!Object.prototype.hasOwnProperty.call(this.value, value)) {
+      throw new PropertyError();
+    }
+
+    return this;
   }
 }
 
--- test/test_ObjectAssert.js
+++ test/test_ObjectAssert.js
@@ -2,6 +2,7 @@ import mocha from 'mocha';
 import chai from 'chai';
 import {ObjectAssert} from '../src/node-assert/index.mjs';
 import {core} from './mocks.js';
+import {PropertyError} from '../src/node-assert/error/index.mjs';
 
 
 const describe = mocha.describe;
@@ -15,5 +16,33 @@ describe('ObjectAssert', () => {
 
     // Then
     expect(core.assertObject.calledWith({})).to.be.true;
+  });
+
+  describe('hasProperty()', () => {
+
+    it('returns an object assert for contained properties', () => {
+      // Given
+      const value = {'key': 'value'};
+      const systemUnderTest = new ObjectAssert(core, value);
+      let requestedProperty = 'key';
+
+      // When
+      const actual = systemUnderTest.hasProperty(requestedProperty);
+
+      // Then
+      expect(actual).to.be.instanceof(ObjectAssert);
+      expect(core.assertString.calledWith(requestedProperty)).to.be.true;
+    });
+
+    it('throws property error if object does not contain property', () => {
+      // Given
+      const value = {'key': 'value'};
+      const systemUnderTest = new ObjectAssert(core, value);
+      let unknownProperty = 'unknown';
+
+      // When/Then
+      expect(() => systemUnderTest.hasProperty(unknownProperty)).to.throw(PropertyError);
+      expect(core.assertString.calledWith(unknownProperty)).to.be.true;
+    });
   });
 });
